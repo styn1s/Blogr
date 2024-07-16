90c314db9a52c4e3c2a76cf0252a7706af578cae
Maksim Yavorets | Front-End Developer <styn1sss@gmail.com>
2024-07-16 14:37:01 +0300

    Mobile menu for tablets

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var connectBtn = document.getElementById("connect-btn");
 var dropdownMenu = document.querySelector(".connect-dropdown");
 var burgerIcon = document.getElementById("burger-menu");
 var mobileMenu = document.querySelector(".mobile-menu");
+var mobileConnBtn = document.getElementById('mobile-connect-btn');
+var mobileDropdown = document.getElementById('mobile-dropdown');
 
 const arrowImage = connectBtn.querySelector('.arrow');
+const mobileArrowImage = mobileConnBtn.querySelector('.arrow');
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -25,4 +28,23 @@ document.addEventListener("DOMContentLoaded", function() {
             mobileMenu.style.display = "flex";
         }
     });
+
+    mobileConnBtn.addEventListener('click', function(event){
+        if (mobileDropdown.style.display === "block"){
+            mobileDropdown.style.display = "none";
+        }
+        else{
+            mobileDropdown.style.display = "block";
+        }
+        mobileArrowImage.classList.toggle('rotate-180');
+    });
 });
+
+window.addEventListener('resize', function(){
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth >= 1000) {
+        mobileMenu.style.display = "none";
+        mobileDropdown.style.display = "none";
+    }
+})
