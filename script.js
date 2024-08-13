e16447331f4c279a5880a25d110357191b706972
Maksim Yavorets <styn1sss@gmail.com>
2024-08-13 16:56:08 +0300

    Fixed desktop and mobile design

--- script.js
+++ script.js
@@ -35,11 +35,14 @@ function toggleMobileDropdown() {
 
 function updateView() {
     var windowWidth = window.innerWidth;
-    if (windowWidth > 1200) {
+    if (windowWidth > 1024) {
         burgerIcon.style.display = "none";
         exitIcon.style.display = "none";
         mobileMenu.style.display = "none";
         mobileDropdown.style.display = "none";
+
+        updateImages("desktop");
+
     } else {
         if (mobileMenu.style.display === "flex") {
             burgerIcon.style.display = "none";
@@ -48,32 +51,48 @@ function updateView() {
             burgerIcon.style.display = "block";
             exitIcon.style.display = "none";
         }
-        updateImages();
+        updateImages("mobile");
+        updateBackground();
     }
+    updateHeading();
 }
 
-function updateImages() {
+function updateImages(imageType) {
     var editorImage = document.getElementById("editor-img");
     var laptopImage = document.getElementById("laptop-pic");
-
-    var editorImagePath = "./images/illustration-editor-mobile.svg";
-    var laptopImagePath = "./images/illustration-laptop-mobile.svg";
-        
+    var editorImagePath = `./images/illustration-editor-${imageType}.svg`;
+    var laptopImagePath = `./images/illustration-laptop-${imageType}.svg`;
     editorImage.src = editorImagePath;
     laptopImage.src = laptopImagePath;
 }
 
+function updateBackground() {
+    var windowWidth = window.innerWidth;
+    var introImage = document.getElementById("intro-pic")
+
+    var imageType = windowWidth <= 768 ? "mobile" : "desktop";
+    var introImagePath = `./images/bg-pattern-intro-${imageType}.svg`;
+    introImage.src = introImagePath;
+}
+
+function updateHeading() {
+    const h1 = document.querySelector('h1');
+    if (window.innerWidth <= 490) {
+        h1.innerHTML = 'A modern<br>publishing platform';
+    } else {
+        h1.innerHTML = 'A modern publishing platform';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    updateView();
+    updateHeading();
     connectBtn.addEventListener("click", function (event) {
         dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
         arrowImage.classList.toggle('rotate-180');
     });
-
     burgerIcon.addEventListener("click", showMobileMenu);
     exitIcon.addEventListener("click", hideMobileMenu);
     mobileConnBtn.addEventListener('click', toggleMobileDropdown);
-
-    updateView();
+    window.addEventListener('resize', updateView);
 });
-
-window.addEventListener('resize', updateView);
